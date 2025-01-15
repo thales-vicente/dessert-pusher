70de485e972003caa2b56cb4f11d09609f6816a6
Thales-Vicente <thalesvicentebem10@hotmail.com>
2025-01-15 16:55:19 -0300

    saveInstance

--- app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertpusher
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -28,6 +29,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleObserver
 import com.example.android.dessertpusher.databinding.ActivityMainBinding
 import timber.log.Timber
+
+const val KEY_REVENUE = "key_revenue"
 
 class MainActivity : AppCompatActivity(), LifecycleObserver {
 
@@ -78,6 +81,10 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
         dessertTimer = DessertTimer(this.lifecycle)
+
+        if (savedInstanceState != null){
+           revenue = savedInstanceState.getInt(KEY_REVENUE)
+        }
 
         // Set the TextViews to the right values
         binding.revenue = revenue
@@ -152,6 +159,12 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             R.id.shareMenuButton -> onShare()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_REVENUE, revenue)
+        Timber.i("onSaveInstanceState Called")
     }
 
     // TODO (02) Override the onStart lifecycle method and add an info level log statement
